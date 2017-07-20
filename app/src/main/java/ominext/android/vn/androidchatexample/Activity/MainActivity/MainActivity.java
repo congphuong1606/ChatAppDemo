8c501115deea5b0804e5a1d852a3f878b0f54ab6
= <=>
2017-07-20 18:29:50 +0700

    vua moi them searchview

--- app/src/main/java/ominext/android/vn/androidchatexample/Activity/MainActivity/MainActivity.java
+++ app/src/main/java/ominext/android/vn/androidchatexample/Activity/MainActivity/MainActivity.java
@@ -6,11 +6,14 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.SearchView;
-import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -20,12 +23,16 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+import java.util.Map;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import de.hdodenhof.circleimageview.CircleImageView;
 import ominext.android.vn.androidchatexample.Activity.MeActivity.Ui.MeActivity;
 import ominext.android.vn.androidchatexample.Adapter.FragmentAdapter;
+import ominext.android.vn.androidchatexample.Adapter.SearchAdapter;
 import ominext.android.vn.androidchatexample.Entities.User;
 import ominext.android.vn.androidchatexample.Instance;
 import ominext.android.vn.androidchatexample.R;
@@ -43,8 +50,16 @@ public class MainActivity extends AppCompatActivity {
     SearchView searchview;
     @BindView(R.id.btn_creat_new_chat)
     ImageButton btnCreatNewChat;
+    @BindView(R.id.rcv_search_view)
+    RecyclerView rcvSearchView;
+    @BindView(R.id.frame_layout_search)
+    FrameLayout frameLayoutSearch;
     private FragmentAdapter adapter;
     FirebaseAuth firebaseAuth;
+    ArrayList<User> users = new ArrayList<>();
+    SearchAdapter searchAdapter;
+    RecyclerView.LayoutManager layoutManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,7 +67,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
         toobar();
-        firebaseAuth=FirebaseAuth.getInstance();
+        rcvSearchViewInit();
+        frameLayoutSearch.setVisibility(View.GONE);
+        firebaseAuth = FirebaseAuth.getInstance();
         adapter = new FragmentAdapter(getSupportFragmentManager());
         frameLayout.setAdapter(adapter);
         frameLayout.setCurrentItem(0);
@@ -85,20 +102,31 @@ public class MainActivity extends AppCompatActivity {
             public boolean onQueryTextChange(String s) {
 
                 DatabaseReference mFirebaseDatabase;
-                mFirebaseDatabase= FirebaseDatabase.getInstance().getReference();
-                String id=firebaseAuth.getCurrentUser().getUid();
-                DatabaseReference reference=mFirebaseDatabase.child(Instance.USERS_PATH);
-                Query query=reference.orderByChild("name")
-                        .startAt(s).endAt(s+"\uf8ff");
+                mFirebaseDatabase = FirebaseDatabase.getInstance().getReference();
+                String id = firebaseAuth.getCurrentUser().getUid();
+                DatabaseReference reference = mFirebaseDatabase.child(Instance.USERS_PATH);
+                Query query = reference.orderByChild("name")
+                        .startAt(s).endAt(s + "\uf8ff");
 
                 query.addValueEventListener(new ValueEventListener() {
                     @Override
                     public void onDataChange(DataSnapshot dataSnapshot) {
-                        if(dataSnapshot!=null) {
+                        if (dataSnapshot != null) {
+                            frameLayoutSearch.setVisibility(View.VISIBLE);
+                            users.clear();
                             for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
                                 User user = postSnapshot.getValue(User.class);
-                                Toast.makeText(MainActivity.this,user.getName(),Toast.LENGTH_LONG).show();
+                                String id = user.getId();
+                                String name = user.getName();
+                                String email=user.getEmail();
+                                String avatar = user.getAvatar();
+                                String phone = user.getPhoneNumber();
+                                boolean online = user.isOnline();
+                                Map<String, Boolean> contacts = user.getContacts();
+                                users.add(new User(id,name,email,avatar,phone,online,contacts));
                             }
+                            searchAdapter.notifyDataSetChanged();
+
                         }
                     }
 
@@ -111,6 +139,14 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    private void rcvSearchViewInit() {
+        layoutManager = new GridLayoutManager(this, 1);
+        rcvSearchView.setLayoutManager(layoutManager);
+        rcvSearchView.setHasFixedSize(true);
+        searchAdapter = new SearchAdapter(this, R.layout.item_user_searched, users);
+        rcvSearchView.setAdapter(searchAdapter);
     }
 
     private void toobar() {
--- app/src/main/java/ominext/android/vn/androidchatexample/Adapter/SearchAdapter.java
+++ app/src/main/java/ominext/android/vn/androidchatexample/Adapter/SearchAdapter.java
@@ -1,40 +1,75 @@
 package ominext.android.vn.androidchatexample.Adapter;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 import java.util.ArrayList;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import de.hdodenhof.circleimageview.CircleImageView;
 import ominext.android.vn.androidchatexample.Entities.User;
+import ominext.android.vn.androidchatexample.R;
 
 /**
  * Created by MyPC on 20/07/2017.
  */
 
 public class SearchAdapter extends RecyclerView.Adapter<SearchAdapter.ViewHolder> {
+    private final Context context;
     ArrayList<User> users = new ArrayList<>();
+
+    public SearchAdapter(Context context, int layout, ArrayList<User> users) {
+        this.users = users;
+        this.context = context;
+    }
+
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_user_searched, parent, false);
+        return new ViewHolder(v);
     }
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-
+        User user = users.get(position);
+        Glide.with(context).load(user.getAvatar())
+                .into(holder.imvAvatarUserSearch);
+        holder.tvSearchName.setText(user.getName());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return users.size();
+    }
+
+    @OnClick({R.id.imv_avatar_user_search, R.id.tv_search_name})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.imv_avatar_user_search:
+                break;
+            case R.id.tv_search_name:
+                break;
+        }
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.imv_avatar_user_search)
+        CircleImageView imvAvatarUserSearch;
+        @BindView(R.id.tv_search_name)
+        TextView tvSearchName;
 
         public ViewHolder(View itemView) {
-
             super(itemView);
+            ButterKnife.bind(this,itemView);
         }
     }
 }
